61892bf8bfd11dec47be75ef7835e667b136887e
softwarenomad <122175570+softwarenomad@users.noreply.github.com>
2023-08-28 20:40:03 -0400

    Update jest.config.cjs
    add new `useESM` flag to jest config

--- jest.config.cjs
+++ jest.config.cjs
@@ -11,6 +11,7 @@ module.exports = {
             {
                 isolatedModules: true,
                 tsconfig: 'tests/tsconfig.json',
+                useESM: true,
             },
         ],
     },
